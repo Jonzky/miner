107d7957b185d89dbf5ca524cb1c7cd91c96ff20
Jon Zakay <JonZakay@aol.com>
2022-02-19 21:44:49 +0000

    Attempt to build miner with a json_rpc method to get the challenger

---
+++ src/jsonrpc/miner_jsonrpc_poc.erl
@@ -0,0 +1,39 @@
+-module(miner_jsonrpc_poc).
+
+-include("miner_jsonrpc.hrl").
+-behavior(miner_jsonrpc_handler).
+-export([handle_rpc/2]).
+
+%%
+%% jsonrpc_handler
+%%
+
+handle_rpc(<<"poc_find">>, #{ <<"key">> := Key }) ->
+
+    POCID = blockchain_utils:poc_id(Key),
+    OnionKeyHash = crypto:hash(sha256, Key),
+    Ledger = blockchain:ledger(blockchain:blockchain()),
+    case blockchain_ledger_v1:find_pocs(OnionKeyHash, Ledger) of
+        {error, not_found} ->
+            not_found;
+        {ok, [PoC]} ->
+            lager:info([{poc_id, POCID}], "found poc. attempting to decrypt", []),
+            try blockchain_ledger_poc_v2:challenger(PoC) of
+                error ->
+                    {error, failed_find_poc};
+                {Challenger} ->
+                    {ok, Challenger}
+            catch C:E:S ->
+                lager:warning([{poc_id, POCID}], "crash during decrypt ~p:~p ~p", [C, E, S]),
+                {error, {C, E}}
+            end;
+            {ok, _} ->
+                {error, too_many_pocs}
+        end,
+    {"testmessage"}
+
+handle_rpc(<<"poc_find">>, Params) ->
+    ?jsonrpc_error({invalid_params, Params});
+
+handle_rpc(_, _) ->
+    ?jsonrpc_error(method_not_found).
