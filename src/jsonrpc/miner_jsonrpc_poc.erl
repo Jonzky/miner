c9a4fb112c6df318c24fa1384aefbd5d230558f7
Jon Zakay <JonZakay@aol.com>
2022-02-20 00:40:27 +0000

    Another poc try

--- src/jsonrpc/miner_jsonrpc_poc.erl
+++ src/jsonrpc/miner_jsonrpc_poc.erl
@@ -19,18 +19,17 @@ handle_rpc(<<"poc_find">>, #{ <<"key">> := Key }) ->
         {ok, [PoC]} ->
             lager:info([{poc_id, POCID}], "found poc. attempting to decrypt", []),
             try 
-                Challanger = blockchain_ledger_poc_v2:challenger(PoC),
-                {ok, Challenger}
-            catch C:E:S ->
-                lager:warning([{poc_id, POCID}], "crash during getting challanger ~p:~p ~p", [C, E, S]),
-                {error, {C, E}}
-            end,
+                {ok, blockchain_ledger_poc_v2:challenger(PoC)}
+            catch errormessage ->
+                lager:warning([{poc_id, POCID}], "crash during getting challanger ~p", [errormessage]),
+                {error, errormessage}
+            end;
         {ok, _} ->
             {error, too_many_pocs}
-    end,
-    ?jsonrpc_error({unable_to_proccess_proc});
+    end.
 
-handle_rpc(<<"poc_find">>, Params) ->
-    ?jsonrpc_error({invalid_params, Params});
+% handle_rpc(<<"poc_find">>, Params) ->
+%     ?jsonrpc_error({invalid_params, Params});
+
 handle_rpc(_, _) ->
     ?jsonrpc_error(method_not_found).
