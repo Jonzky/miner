eba8ed0fb17b8f621f3c2381ee9ba70c2ab132f3
Jon Zakay <JonZakay@aol.com>
2022-02-21 18:07:51 +0000

    Attempt to parse packet and extract payload

--- src/jsonrpc/miner_jsonrpc_poc.erl
+++ src/jsonrpc/miner_jsonrpc_poc.erl
@@ -9,29 +9,36 @@
 %%
 
 
-handle_rpc(<<"poc_find">>, #{ <<"key">> := DataMessage }) ->
+handle_rpc(<<"poc_find">>, #{ <<"key">> := DataPacket }) ->
     try
-        lager:info([{poc_id}], "Request received to get PoC - ~p", [DataMessage]),
-        BinMessage = ?B64_TO_BIN(DataMessage),
-        BinKey = get_onion_key(BinMessage),                            
-        POCID = blockchain_utils:poc_id(BinKey),
-        OnionKeyHash = crypto:hash(sha256, BinKey),
-        lager:info([{poc_id}], "Getting Blockchain - ~p", [DataMessage]),
-        Ledger = blockchain:ledger(blockchain:blockchain()),
-        lager:info([{poc_id}], "Looking it up - ~p", [DataMessage]),
-        case blockchain_ledger_v1:find_pocs(OnionKeyHash, Ledger) of
-            {error, not_found} ->
-                not_found;
-            {ok, [PoC]} ->
-                lager:info([{poc_id, POCID}], "found poc. attempting to decrypt", []),
-                Challanger = blockchain_ledger_poc_v2:challenger(PoC),
-                {ok, Challanger};
-            {ok, _} ->
-                {error, too_many_pocs}
-        end
+        lager:info([{poc_id}], "Request received to get PoC - ~p", [DataPacket]),
+
+        case get_payload(DataPacket) of 
+
+            {ok, Payload} ->
+                BinKey = get_onion_key(BinMessage),                            
+                POCID = blockchain_utils:poc_id(BinKey),
+                OnionKeyHash = crypto:hash(sha256, BinKey),
+                lager:info([{poc_id}], "Getting Blockchain - ~p", [DataPacket]),
+                Ledger = blockchain:ledger(blockchain:blockchain()),
+                lager:info([{poc_id}], "Looking it up - ~p", [DataPacket]),
+                case blockchain_ledger_v1:find_pocs(OnionKeyHash, Ledger) of
+                    {error, not_found} ->
+                        not_found;
+                    {ok, [PoC]} ->
+                        lager:info([{poc_id, POCID}], "found poc. attempting to decrypt", []),
+                        Challanger = blockchain_ledger_poc_v2:challenger(PoC),
+                        {ok, Challanger};
+                    {ok, _} ->
+                        {error, too_many_pocs}
+                end;
+            {error, _} ->
+                lager:error([{poc_id}], "Failed proccess data packet - ~p", [DataMessage]),
+                ?jsonrpc_error({failed_proccess_packet, DataMessage})
+        end;
     catch
         _:_ ->
-            lager:info([{poc_id}], "Failed to get PoC - ~p", [DataMessage]),
+            lager:error([{poc_id}], "Failed to get PoC - ~p", [DataMessage]),
             ?jsonrpc_error({invalid_params, DataMessage})
     end;
 
@@ -42,5 +49,17 @@ handle_rpc(<<"poc_find">>, Params) ->
 handle_rpc(_, _) ->
     ?jsonrpc_error(method_not_found).
 
+
+get_payload(DataPacket) ->
+    case miner_lora:route(DataPacket) of
+        error ->
+            {error, failure};
+        {onion, Payload} ->
+            {ok, Payload};
+        {noop, non_longfi} ->
+            lager:debug("Unable to get PoC on non Lofi packet ~p", [DataPacket])
+    end,
+    {error, failure}.
+    
 get_onion_key({ <<_:2/binary, OnionCompactKey:33/binary>>}) ->
                                   {OnionCompactKey}.
